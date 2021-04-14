b2bd63cb3a35479b60f88239e8e7eed082d1094a
Raghad Al Samrout <raghadsamrout@ieee.org>
2021-04-14 11:34:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
+
 function wakeDog(dogName, dogBreed) {
-
-console.log(`Wake ${dogName} the ${dogBreed}`);
-
+    return (`Wake ${dogName} the ${dogBreed}`);
 }
-function leashDog(dogNmae,dogBreed){
-let String=
-  console.log(`Leash ${dogName} the ${dogBreed}`);
-  return String;
+function leashDog(dogName, dogBreed) {
+  return (`Leash ${dogName} the ${dogBreed}`);
+}
+function walkToPark(dogName, dogBreed) {
+  return (`Walk to the park with ${dogName} the ${dogBreed}`);
+}
+function throwFrisbee(dogName, dogBreed) {
+  return (`Throw the frisbee for ${dogName} the ${dogBreed}`);
+}
+function walkHome(dogName, dogBreed) {
+  return (`Walk home with ${dogName} the ${dogBreed}`);
+}
+function unleashDog(dogName, dogBreed) {
+  return (`Unleash ${dogName} the ${dogBreed}`);
+}
+let routine = [wakeDog, leashDog, walkToPark, throwFrisbee, walkHome, unleashDog];
+function exerciseDog(dogName, dogBreed) {
+  return routine.map(fn => fn(dog, breed))
 }
