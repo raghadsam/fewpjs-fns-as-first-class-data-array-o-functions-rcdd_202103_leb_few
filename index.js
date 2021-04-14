86e487e353007076e0042e20cbabcf3795f4bf01
Raghad Al Samrout <raghadsamrout@ieee.org>
2021-04-14 11:31:26 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,10 @@
+function wakeDog(dogName, dogBreed) {
+
+console.log(`Wake ${dogName} the ${dogBreed}`);
+
+}
+function leashDog(dogNmae,dogBreed){
+let String=
+  console.log(`Leash ${dogName} the ${dogBreed}`);
+  return String;
+}
